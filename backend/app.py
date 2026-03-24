a80a48d6cd34a1ed4e8bcf1523036a9387991d3b
Rhitik Prajapati <rhitikprajapati9279@gmail.com>
2026-03-24 15:11:33 +0000

    frontend ready for deployment

--- backend/app.py
+++ backend/app.py
@@ -464,4 +464,5 @@ def home():
 # RUN SERVER
 # -------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
